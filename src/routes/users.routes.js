a643693c5e929ec02ca2ac5fec945353eee05974
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-01-30 11:33:39 -0300

    login via jwt auth - dotenv key

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const secretToken = process.env.JWT_TOKEN
+
 const User = require('../models/entities/user')
 
 router.post('/register', async (request, response) => {
@@ -14,6 +18,33 @@ router.post('/register', async (request, response) => {
   
   catch (error) {
     response.status(500).json({ error: '500: Error on attempting to register new user' })
+  }
+})
+
+router.post('/login', async (request, response) => {
+  const { email, password } = request.body
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (!user)
+      response.status(401).json({ error: 'Incorrect email or password' })
+
+    else {
+      user.isCorrectPassword(password, function(error, same) {
+        if (!same)
+          response.status(401).json({ error: 'Incorrect email or password' })
+
+        else {
+          const token = jwt.sign({ email }, secretToken, { expiresIn: '30d' })
+          response.json({ user: user, token: token })
+        }
+      })
+    }
+  } 
+  
+  catch (error) {
+    response.status(500).json({ error: '500: Internal error, please try again' })
   }
 })
 

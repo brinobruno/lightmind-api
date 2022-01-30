4a9a50b90c6df1b91cf3450df9ab52bda518492e
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-01-30 11:14:48 -0300

    register user route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(request, response, next) {
-  response.send('respond with a resource');
-});
+const User = require('../models/entities/user')
+
+router.post('/register', async (request, response) => {
+  const { name, email, password } = request.body
+  const user = new User({ name, email, password })
+
+  try {
+    await user.save()
+    response.status(201).json(user)
+  } 
+  
+  catch (error) {
+    response.status(500).json({ error: '500: Error on attempting to register new user' })
+  }
+})
 
 module.exports = router;

753ef8c166614d646ec7504e01ad11864b053ba4
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-01-30 22:01:53 -0300

    refactor routes controllers inside controllers folder

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,8 +5,6 @@ var router = express.Router();
 const registerUser = require('../controllers/users/register')
 const loginUser = require('../controllers/users/login')
 
-require('dotenv').config()
-
 router.post('/register', registerUser)
 
 router.post('/login', loginUser)

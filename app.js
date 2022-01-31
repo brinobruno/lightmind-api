c259e45d2878ca1fcb37caf3b067ec3869635599
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-01-31 16:28:37 -0300

    implement cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+const cors = require('cors')
 
 require('./src/models/db/database.config')
 
@@ -13,6 +14,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
 
 //routes
 app.use('/users', usersRouter);
